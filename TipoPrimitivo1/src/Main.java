08e03a57ba17d45c602c0de5250d2099d05656f7
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-06-17 12:53:34 -0300

    Add files via upload

---
+++ TipoPrimitivo1/src/Main.java
@@ -0,0 +1,10 @@
+//Posso utilizar printf ou format
+//%.1f é a casa decimal
+public class Main {
+    public static void main(String[] args) {
+        String nome= "Ana Júlia";
+        float nota = 8.5f;
+        System.out.format("A nota de %s é %.1f \n", nome, nota);
+
+    }
+}
---
+++ TiposPrimitivos/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        float nota = 8.5f;
+        System.out.println("A nota é " + nota);
+    }
+}

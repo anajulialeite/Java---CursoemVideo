a5184094b566deb06c9a06db53a68258737ca030
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-06-18 16:14:21 -0300

    Add files via upload

---
+++ OperadoresAritmeticos6/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double ale = Math.random(); //ale de aleatório
+        int n = (int) (15 + ale * (50-15));
+        System.out.println(n);
+
+        //para fazer somente números aleatórios
+        /*double ale = Math.random();
+          System.out.println(ale);*/
+    }
+}

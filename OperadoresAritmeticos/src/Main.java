cd2df9451ca6713053186116039a948df16800eb
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-06-18 14:12:23 -0300

    Add files via upload

---
+++ OperadoresAritmeticos/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual a " + m);
+    }
+}
